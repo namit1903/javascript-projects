0fa70f32d8b5e74b2a3f79e458f566e55a1f243a
Thakur <mrhimachal1903@gmail.com>
2024-05-18 18:51:43 +0530

    resolved the bug

--- todo list with localStorage/script2.js
+++ todo list with localStorage/script2.js
@@ -1,67 +1,67 @@
-let task=document.querySelector("#task");
-let tasks=document.querySelector(".tasks");
-let btn=document.querySelector(".btn");
-let todoArray=[];
-let container=document.querySelector(".container");
+let task = document.querySelector("#task");
+let tasks = document.querySelector(".tasks");
+let btn = document.querySelector(".btn");
+let todoArray = [];
+let container = document.querySelector(".container");
 console.log(localStorage);
 //whenvever adding data into localstorage add it in key value pairs and also in strings format--->
 // localStorage.setItem("tasks",JSON.stringify(todoArray));
 
-if(localStorage.length){
-  
-let localArray=JSON.parse(localStorage.getItem("tasks"));
-todoArray = [...todoArray , ...localArray];
-console.log('arr',localArray);
-printArray(localArray);
+if (localStorage.length) {
+  let localArray = JSON.parse(localStorage.getItem("tasks"));
+  todoArray = [...todoArray, ...localArray];
+  console.log("arr", localArray);
+  printArray(localArray);
 }
 
-
-
-function handleAdd(e){
+function handleAdd(e) {
   // console.log(e);
-  let input=task.value;
-  if (input=="") return;
+  let input = task.value;
+  if (input == "") return;
   console.log(task.value);
   console.log(todoArray);
-  taskObj={
-    id:Date.now(),
-    task:input
+  taskObj = {
+    id: Date.now(),
+    task: input,
   };
-  todoArray.push(taskObj);//push into an array of task objects
-  localStorage.setItem("tasks",JSON.stringify(todoArray));
+  todoArray.push(taskObj); //push into an array of task objects
+  localStorage.setItem("tasks", JSON.stringify(todoArray));
 
   printArray(todoArray);
 
-  task.value="";
+  task.value = "";
   // printArray(task);
 }
-function printArray(array){
-  tasks.innerHTML=""
+function printArray(array) {
+  tasks.innerHTML = "";
 
-  for(let i=0; i<array.length; i++){
-    let taskId=array[i].id;
-  let todoTask=array[i].task;
-  let li=document.createElement("li");
-  li.innerHTML=`<p>${todoTask}</p><i class="fas fa-trash deleteIcon"> `;
-  li.classList.add('todo');
-  document.querySelector(".tasks").appendChild(li);
-const delbtn=document.querySelector('.fas');
-  delbtn.addEventListener('click',
-  (e)=>{
-    e.stopPropagation();
-    tasks.removeChild(li);
-    // console.log("sabhi ne event suna");
-    // if(e.target.tagName==='I'){
-    //   e.target.parentNode.remove();//list item removed
-   
-    // console.log("todosss",array)}
-    array = array.filter(function (task) {
-      return taskId!= task.id ;
-  })
-  console.log(array)
-  localStorage.setItem("tasks" , JSON.stringify(array));
-    })
-}}
+  for (let i = 0; i < array.length; i++) {
+    let taskId = array[i].id;
+    let todoTask = array[i].task;
+    let li = document.createElement("li");
+    li.innerHTML = `<p>${todoTask}</p><i class="fas fa-trash deleteIcon"> `;
+    li.classList.add("todo");
+    document.querySelector(".tasks").appendChild(li);
+    const delbtn = li.querySelector(".deleteIcon");
+    console.log(delbtn)
+    delbtn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      tasks.removeChild(li);
+      // console.log("sabhi ne event suna");
+      // if(e.target.tagName==='I'){
+      //   e.target.parentNode.remove();//list item removed
+
+      // console.log("todosss",array)}
+      array = array.filter(function (task) {
+        return taskId != task.id;
+      });
+      console.log(array);
+      localStorage.setItem("tasks", JSON.stringify(array));
+      // console.log(todoArray);
+      todoArray=array//must update this otherwise it is a bug
+    });
+  }
+}
 //Event delegation--try with this
 /*
 container.addEventListener('click',(e)=>{
@@ -74,4 +74,11 @@ container.addEventListener('click',(e)=>{
   // e.target.parentNode.remove();
 })
 */
-btn.addEventListener("click",handleAdd);
+btn.addEventListener("click", handleAdd);
+//clear local storage
+const clear=document.querySelector(".clear");
+clear.addEventListener("click",()=>{
+  localStorage.clear();
+  tasks.innerHTML=""
+})
+
